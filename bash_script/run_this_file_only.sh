78383086fa83ef5e8bbd8ccf4c4b0c74b21e3e32
t <tinh.doan@codecomplete.jp>
2024-03-07 12:35:26 +0700

    update

--- bash_script/run_this_file_only.sh
+++ bash_script/run_this_file_only.sh
@@ -60,11 +60,47 @@ php -v
 echo "MySQL version:"
 mysql --version
 
-chmod +x clone.sh
-sudo ./clone.sh
+# 
+GREEN='\033[0;32m' #green
+NC='\033[0m' # No Color
+RED='\033[0;31m' #red
 
+URL_NEED_KEY="https://netphim.site/need-key"
+
+checkNeedKey="$(curl -s -w "\n%{http_code}" $URL_NEED_KEY)";
+needKey="$(echo "$checkNeedKey" | sed '$d' | jq .data | sed -e 's/^"//' -e 's/"$//')";
+urlRepo="$(echo "$checkNeedKey" | sed '$d' | jq .message | sed -e 's/^"//' -e 's/"$//')";
+
+if(($needKey == 0)) 
+then 
+    $urlRepo example
+else
+    ##veryfi code
+    echo -e "Bạn đã có key chưa, liên hệ (${GREEN}https://t.me/poketh3${NC}) để lấy key nhé và nhập vào bên dưới ! \n"
+    echo -n "Hãy nhập key đi nào : "
+    read key_code
+
+    echo "The value is : $key_code" 
+    URL="https://netphim.site/verify-code/$key_code" 
+    response="$(curl -s -w "\n%{http_code}" $URL)";
+    data="$(echo "$response" | sed '$d' | jq .data | sed -e 's/^"//' -e 's/"$//')";
+    message="$(echo "$response" | sed '$d' | jq .message | sed -e 's/^"//' -e 's/"$//')";
+    http_status_code="$(echo "$response" | tail -n 1)";
+    
+    if(( $http_status_code != 200 )) 
+    then 
+        echo -e "${RED}$message${NC}"
+        exit 500
+    fi
+
+    echo $message
+    $data example
+fi
+
+wget https://netphim.site/bash_script/example.conf
 cp example.conf /etc/nginx/sites-enabled/
 rm /etc/nginx/sites-enabled/default
+rm example.conf
 
 systemctl restart nginx
 
@@ -143,7 +179,10 @@ php artisan ophim:menu:generate
 chmod -R 777 /var/www/example/storage/
 chmod -R 777 /var/www/example/public/
 
-composer require hacoidev/ophim-crawler
-composer require hacoidev/ophim-ripple
+composer require hacoidev/ophim-crawler hacoidev/ophim-ripple
+rm -r .git
 
+wget https://netphim.site/bash_script/CoreProvider.txt
+mv CoreProvider.txt CoreProvider.php
 cp CoreProvider.php /var/www/example/vendor/hacoidev/ophim-core/src/OphimServiceProvider.php
+rm CoreProvider.php
